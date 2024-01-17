79f6865283db038b5bb382473113a7f0e4edb081
Joris Verstegen <39861914+BigInteger28@users.noreply.github.com>
2024-01-17 20:50:38 +0100

    Update renteberekening.go

--- renteberekening.go
+++ renteberekening.go
@@ -5,29 +5,39 @@ import (
 )
 
 func main() {
-	startkapitaal := 2000.0
+	startkapitaal := 1000.0
 	maandelijksBedrag := 500.0
 	basisrente := 0.9 / 100
 	getrouwheidspremie := 1.65 / 100
-	aantalJaar := 5 // Aantal jaren voor renteberekening
+	aantalJaar := 10
+	aantalExtraMaanden := 0 // De gebruiker kan dit wijzigen naar wens
 
-	totaalSaldo := float64(startkapitaal) // Begin met het startkapitaal
+	totaalSaldo := startkapitaal
+	totaleStortingen := startkapitaal // Begin met het startkapitaal
 
-	for jaar := 1; jaar <= aantalJaar; jaar++ {
-		for maand := 1; maand <= 12; maand++ {
-			// Voeg maandelijks bedrag toe aan totaalSaldo
-			totaalSaldo += float64(maandelijksBedrag)
+	// Bereken het totaal aantal maanden
+	totaalAantalMaanden := aantalJaar*12 + aantalExtraMaanden
 
-			if jaar > 1 && maand%3 == 0 {
-				// Voeg getrouwheidspremie toe elke 3 maanden vanaf het tweede jaar
-				totaalSaldo += totaalSaldo * getrouwheidspremie
-			}
+	for maand := 1; maand <= totaalAantalMaanden; maand++ {
+		// Voeg maandelijks bedrag toe aan totaalSaldo en totaleStortingen
+		totaalSaldo += maandelijksBedrag
+		totaleStortingen += maandelijksBedrag
+
+		// Bereken getrouwheidspremie elke 3 maanden vanaf het tweede jaar
+		if maand > 12 && maand%3 == 0 {
+			totaalSaldo += totaalSaldo * getrouwheidspremie
 		}
 
-		// Bereken en voeg basisrente toe aan het einde van elk jaar
-		totaleBasisrente := totaalSaldo * basisrente
-		totaalSaldo += totaleBasisrente
-
-		fmt.Printf("Jaar %d: Totale saldo is €%.2f\n", jaar, totaalSaldo)
+		// Voeg basisrente toe aan het einde van elk jaar
+		if maand%12 == 0 {
+			totaleBasisrente := totaalSaldo * basisrente
+			totaalSaldo += totaleBasisrente
+		}
 	}
+
+	// Bereken en print de resultaten
+	winst := totaalSaldo - totaleStortingen
+	fmt.Printf("Totaal gespaarde bedrag na %d jaar en %d maanden is €%.2f\n", aantalJaar, aantalExtraMaanden, totaalSaldo)
+	fmt.Printf("Totaal gestort bedrag na %d jaar en %d maanden is €%.2f\n", aantalJaar, aantalExtraMaanden, totaleStortingen)
+	fmt.Printf("Totale winst na %d jaar en %d maanden is €%.2f\n", aantalJaar, aantalExtraMaanden, winst)
 }
